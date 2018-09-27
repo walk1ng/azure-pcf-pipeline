0db22421594b87b8ee2e82cb695afd1fc80c4411
Wei Li (Wicresoft) <v-lii@microsoft.com>
2018-09-27 11:39:30 +0800

    add job for build bosh azure cpi

---
+++ tasks/build-cpi/task.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "=============================================================================================="
+echo "Building BOSH Azure CPI release on ${CPI_SOURCE_BRANCH} branch ..."
+echo "=============================================================================================="
+
+export TERM=xterm
+cd resource-bosh-cpi-src/
+echo "Check ${CPI_SOURCE_BRANCH} branch"
+git checkout ${CPI_SOURCE_BRANCH}
+git log -3
+cpi_release_name="bosh-azure-cpi"
+timestamp=`date +%s`
+echo "Creating release"
+bosh create-release --name ${cpi_release_name} \
+ --version ${CPI_SOURCE_BRANCH}-${timestamp} \
+ --tarball ../builds/${cpi_release_name}-${CPI_SOURCE_BRANCH}.tgz
