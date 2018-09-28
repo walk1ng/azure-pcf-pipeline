b6268a7ff99199f567ae6e05d071307134c6ca71
Wei Li (Wicresoft) <iliwgg@gmail.com>
2018-09-28 16:56:55 +0800

    add job for deploy director

---
+++ tasks/deploy-director/task.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# get values from terraform.tfstate
+OPSMAN_DOMAIN_OR_IP_ADDRESS=`terraform output -state=./resource-terraform-tfstate/terraform.tfstate -json | jq -r '.ops_manager_public_ip.value'`
+
+echo "=============================================================================================="
+echo "Deploying Director @ https://${OPSMAN_DOMAIN_OR_IP_ADDRESS} ..."
+echo "=============================================================================================="
+
+# Apply Changes in Opsman
+
+om-linux --target https://${OPSMAN_DOMAIN_OR_IP_ADDRESS} -k \
+  --username "${PCF_OPSMAN_ADMIN}" \
+  --password "${PCF_OPSMAN_ADMIN_PASSWORD}" \
+  apply-changes
